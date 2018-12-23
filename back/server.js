d8f7ca2390713d66cacc2c9c539f7a744f9aa599
Marco <marco.mojica@hotmail.de>
2018-12-23 01:03:45 +0100

    Add external redis hosting

--- back/server.js
+++ back/server.js
@@ -1,12 +1,14 @@
 const server = require("http").createServer()
 const io = require("socket.io")(server, {serveClient: false})
 
-const REDIS_HOST = "nowv2testredis.now.sh"
+const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_PORT = process.env.REDIS_PORT
+const REDIS_PASS = process.env.REDIS_PASS
 
 const redis = require('redis')
 const redisAdapter = require('socket.io-redis')
-const pub = redis.createClient(REDIS_HOST, { auth_pass: "StrongPassword!!!" })
-const sub = redis.createClient(REDIS_HOST, { auth_pass: "StrongPassword!!!" })
+const pub = redis.createClient(REDIS_PORT, REDIS_HOST, { auth_pass: REDIS_PASS })
+const sub = redis.createClient(REDIS_PORT, REDIS_HOST, { auth_pass: REDIS_PASS })
 io.adapter(redisAdapter({ pubClient: pub, subClient: sub }))
 
 io.on("connect", socket => {
